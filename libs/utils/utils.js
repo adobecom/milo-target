f4a090f24b6fbc50c54fb186c3746f548a762519
Chris Peyer <cpeyer@adobe.com>
2023-02-10 13:57:07 -0500

    Support martech=off

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -623,7 +623,9 @@ async function loadMartech(config) {
     performance.mark('after-martech-call');
     const r = performance.measure('runmartech', 'after-martech-load', 'after-martech-call');
     console.log('runmartech', r.duration);
+    return true;
   }
+  return false;
 }
 
 async function loadPostLCP(config) {
@@ -842,15 +844,17 @@ export async function loadArea(area = document) {
 
 
   if (isDoc) {
-    await loadMartech(config);
-    const experiment = await checkForExperiments();
-    if (experiment) {
-      setConfig({ ...getConfig(), experiment });
+    const martechIsRunning = await loadMartech(config);
+    if (martechIsRunning) {
+      const experiment = await checkForExperiments();
+      if (experiment) {
+        setConfig({ ...getConfig(), experiment });
+      }
+      const beforeExp = performance.measure('before-running-exp', 'loadpage', 'start-runexperiment');
+      const runExp = performance.measure('runexperiment', 'start-runexperiment', 'finish-runexperiment');
+      console.log('beforeExp', beforeExp.duration);
+      console.log('runExp', runExp.duration);
     }
-    const beforeExp = performance.measure('before-running-exp', 'loadpage', 'start-runexperiment');
-    const runExp = performance.measure('runexperiment', 'start-runexperiment', 'finish-runexperiment');
-    console.log('beforeExp', beforeExp.duration);
-    console.log('runExp', runExp.duration);
   }
 
 
@@ -931,7 +935,7 @@ export function loadDelayed(delay = 3000) {
       }
       import('./samplerum.js').then(({ sampleRUM }) => sampleRUM('cwv'));
 
-      if (window.location.hostname.endsWith('hlx.page') || window.location.hostname === ('localhost')) {
+      if ((window.location.hostname.endsWith('hlx.page') || window.location.hostname === ('localhost')) && window.hlx.experiment) {
         import('../scripts/preview/preview.js');
       }
     }, delay);
