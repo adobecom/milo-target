79f05a11d6957683bdae292981846e8e51f737d3
vivgoodrich <vivian.goodrich@gmail.com>
2023-03-01 18:28:06 -0700

    restore

--- libs/scripts/scripts.js
+++ libs/scripts/scripts.js
@@ -11,9 +11,6 @@
  */
 
 import { loadArea, loadDelayed, loadLana, setConfig } from '../utils/utils.js';
-import('../scripts/experiments.js');
-import('/libs/utils/decorate.js');
-import('/libs/martech/attributes.js');
 
 // Production Domain
 const prodDomains = ['milo.adobe.com'];
